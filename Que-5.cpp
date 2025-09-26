665bcf418e4d6e5a31231f41751c8eb7bc620eb4
ABHINAV TIWARI <TIWARIABHINAV273@GMAIL.COM>
2025-09-26 19:32:08 +0530

    Update Que-5.cpp
    Positive or Negative Number

--- Que-5.cpp
+++ Que-5.cpp
@@ -1,1 +1,18 @@
+/* Write a program to get a number from the user and print whether it is positive or negative*/
+#include <iostream>
+using namespace std;
+int main() {
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
 
+    if (number > 0) {
+        cout << "The number is positive." << endl;
+    } else if (number < 0) {
+        cout << "The number is negative." << endl;
+    } else {
+        cout << "The number is zero." << endl;
+    }
+
+    return 0;
+}
