7ea69119c12e4afa63a507737dd945f305f536d3
ABHINAV TIWARI <TIWARIABHINAV273@GMAIL.COM>
2025-09-26 17:26:51 +0530

    cpp-qs 1

---
+++ main.cpp
@@ -0,0 +1,13 @@
+//cpp// 
+//Que 1 - Input Size of square = n , get output = n*n .//
+  //solution//
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    int area = n*n;
+    cout << "Enter n: ";
+    cin >> n;
+    cout << "Area of square is: " << n*n << endl;
+return 0;
+}
