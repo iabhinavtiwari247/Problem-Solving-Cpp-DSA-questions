d86c8a05dee3062b2820e6cfa2869ae0e0d48b31
ABHINAV TIWARI <TIWARIABHINAV273@GMAIL.COM>
2025-10-09 01:34:38 +0530

    Update and rename hackerrank-q1 to hackerrank-q1.cpp

--- hackerrank-q1.cpp
+++ hackerrank-q1.cpp
@@ -1,5 +1,5 @@
 //cpp//
-"You are familiar with producing output . In this task, you'll be required to write three messages in three separate lines. Write Geeks for Geeks. But wait a minute!! You need to write each word of it in a separate line."
+//que- You are familiar with producing output . In this task, you'll be required to write three messages in three separate lines. Write Geeks for Geeks. But wait a minute!! You need to write each word of it in a separate line//.
 
 Example :
 
